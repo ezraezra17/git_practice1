6fb2efb96c3d76c9cee8fec0005c8ec9bc45cf74
eshazine <eshazine@gmail.com>
2022-04-13 22:38:16 +0300

    made amendments

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 // Doing aweseome work 3
     //Awesome work 3 is done
     // and ready to be pushed to remote repository
-
+public static void main(String[] args){
+    System.out.println("Hello world");
+}
 }
